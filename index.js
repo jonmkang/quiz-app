3d3fac846dfb163c8ec2994b26707557be70bf89
Jonathan Kang <jon.m.kang@gmail.com>
2020-04-10 20:00:42 -0400

    added user comments

--- index.js
+++ index.js
@@ -47,11 +47,13 @@ function startQuiz(questionsAnswered = 0, questionNumber = 1, numberCorrect = 0)
     }
 };
 
+//This function removes previous question number label and adds a new one for each question
 function printQuestionNumber(number){
     $('.question-number').remove();
     $('main').prepend(`<p class="question-number">Question ${number}</p>`);
 };
 
+//This function adds the number of questions correctly answered out of total questions answered
 function printTotalCorrect(questionNumber, numberCorrect){
     if(questionNumber != 0){
     $('.total-correct').remove();
@@ -59,32 +61,34 @@ function printTotalCorrect(questionNumber, numberCorrect){
     };
 };
 
+//This function loads the current question at index from the array given
 function loadQuestion(currentQuestion){
-    const questionProduced = produceQuestion();
-    printQuestion(questionProduced[currentQuestion].question);
+    printQuestion(produceQuestion()[currentQuestion].question);
 };
 
+//This function adds HTML code to the quiz-box 
 function printQuestion(question){
-    const questionText = generateQuestionText(question);
     $('.quiz-box').append(`<div class="question-box"></div>`);
-    $('.question-box').append(questionText);
+    $('.question-box').append(generateQuestionText(question));
 };
 
+//This function creates an element for a given question
 function generateQuestionText(questionToPrint){
     return `<div class="question">${questionToPrint}</div>`;
 };
 
+//This function loads the current questions choices to print
 function loadChoices(currentQuestion){
-    const choicesProduced = produceQuestion();
-    printChoices(choicesProduced[currentQuestion].choices);
+    printChoices(produceQuestion()[currentQuestion].choices);
 };
 
+//This function adds a form and the questions choices to the HTML doc
 function printChoices(choices){
-    const choicesText = generateChoicesText(choices);
     $('.quiz-box').append(`<form action="submit" class='js-choices'></form>`)
-    $('.js-choices').append(choicesText);
+    $('.js-choices').append(generateChoicesText(choices));
 };
 
+//This function iterates thru the choices array given and returns radio buttons for each choice.
 function generateChoicesText(choicesToPrint){
     let choices = [];
     let i = 0;
@@ -97,56 +101,58 @@ function generateChoicesText(choicesToPrint){
     return choices;
 };
 
+//This function checks if a radio button is checked, submits the value, and allows the user to progess to the next question.
 function submitAnswer(answered, number, numCorrect){
     createSubmitButton();
     highlightChoice();
-        $('.quiz-box').on("click", ".submit-answer", function(event){
-            event.preventDefault();
-            if($("input[name='choice']:checked").val()){
-                // console.log(checkAnswer($("input[name='choice']:checked").val(), loadAnswer(answered)));
-                if(checkAnswer($("input[name='choice']:checked").val(), loadAnswer(answered))){
-                    ifCorrect(loadAnswer(answered));
-                    numCorrect ++;
-                } else{
-                    ifIncorrect(loadAnswer(answered));
-                }
-                answered ++;
-                number ++;
+    $('.quiz-box').on("click", ".submit-answer", function(event){
+        event.preventDefault();
+        if($("input[name='choice']:checked").val()){
+            if(checkAnswer($("input[name='choice']:checked").val(), loadAnswer(answered))){
+                ifCorrect(loadAnswer(answered));
+                numCorrect ++;
+            } else{
+                ifIncorrect(loadAnswer(answered));
+            }
+            answered ++;
+            number ++;
                 
                 
-                $('.quiz-box').on("click", ".next-question", function(event){
-                    clearQuizBox();
-                    startQuiz(answered, number, numCorrect);
-                });
+            $('.quiz-box').on("click", ".next-question", function(event){
+                clearQuizBox();
+                startQuiz(answered, number, numCorrect);
+            });
                 
-            };
-        });
+        };
+    });
 
 };
 
+//This function creates a submit button
 function createSubmitButton(){
     $('.js-choices').append(`<button type="submit" class="submit-answer">Submit answer</button>`);
 };
 
+//This function clears the quiz-box container
 function clearQuizBox(){
     $('.quiz-box').empty();
 };
 
-
+//This question returns the answer of the question index given
 function loadAnswer(questionIndex){
     return produceQuestion()[questionIndex].answer;
 };
 
+//This function checks if the answer given matches the answer key.
 function checkAnswer(userAnswer, answerKey){
     if(userAnswer == answerKey){
-        console.log(userAnswer);
-        console.log(answerKey);
         return 1;
     } else {
         return 0;
     }
 }
 
+//This function provides the correct answer and prints a sorry statement because the answer given was incorrect.
 function ifIncorrect(correctAns){
     clearQuizBox();
     $('.quiz-box').append(`<div class="question-box"></div>`);
@@ -154,6 +160,7 @@ function ifIncorrect(correctAns){
     nextQuestionButton();
 }
 
+//This function congratulates the user on submitting a correct answer and prints the correct answer.
 function ifCorrect(correctAns){
     clearQuizBox();
     $('.quiz-box').append(`<div class="question-box"></div>`);
@@ -161,10 +168,12 @@ function ifCorrect(correctAns){
     nextQuestionButton();
 }
 
+//This function creates a button allowing the user to progess to the next question.
 function nextQuestionButton(){
     $('.quiz-box').append(`<button type="button" class="next-question">Next question</button>`);
 };
 
+//This function highlights the selected choice by the user.
 function highlightChoice(){
     $(".choice").click(function(event){
         $('.choice').removeClass("choice-selected");
@@ -172,6 +181,7 @@ function highlightChoice(){
     });
 };
 
+//This function clears the question number and total answered, prints a statement showing the user how many they got correct, and allows the user to try again.
 function printResults(questionsAnswered, questionNumber, numberCorrect){
     $('.question-number').remove();
     $('.total-correct').remove();
@@ -180,6 +190,7 @@ function printResults(questionsAnswered, questionNumber, numberCorrect){
     restartButton();
 };
 
+//This function creates a button to start the quiz over.
 function restartButton(){
     $('.quiz-box').append(`<button type="button" class="restartButton">Let me try again!</button>`);
     $('.quiz-box').on('click', ".restartButton", function(event){
